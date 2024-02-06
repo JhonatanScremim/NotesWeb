1e9ffca3abc21acaecfc1e492a866861637d23cc
Jhonatan <jhonatanscremim@gmail.com>
2024-02-06 09:06:46 -0300

    feat(interface)
    Começa a criação da interface principal

--- notes-web/src/app.tsx
+++ notes-web/src/app.tsx
@@ -1,5 +1,29 @@
+import logo from "./assets/logo-nlw.svg";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-cart";
+
+// space-y-6: Adiciona espaçamento de 6 em todos os elementos da div
+// h-px: altura com 1 px, apenas para criar a linha na página
+// outline-none: Retira borda automatica do navegador ao selecionar um input ou outro elemento
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  )
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo" />
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          type="text"
+          placeholder="Busque em suas notas..."
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />  
+        <NoteCard />  
+        <NoteCard />  
+      </div>
+    </div>
+  );
 }
