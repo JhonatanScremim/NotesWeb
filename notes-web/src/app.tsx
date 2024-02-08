1dc132fad1cf2f71bc1926e3ba0faa466f01a740
Jhonatan <jhonatanscremim@gmail.com>
2024-02-08 08:43:05 -0300

    feat(local-storage)
    Adiciona função para utilizar o local storage para salvar as notas

--- notes-web/src/app.tsx
+++ notes-web/src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
@@ -14,7 +14,17 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [search, setSearch] = useState("");
+
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem("notes");
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage);
+    }
+
+    return [];
+  });
 
   function onNoteCreated(content: string) {
     const newNote = {
@@ -25,8 +35,24 @@ export function App() {
 
     // Quando você quer atualizar as notas do useState, não pode substituir o que já está lá
     // Então deve adicionar a nova nota, e depois copiar as outras que já estavam no useState, ou vice e versa
-    setNotes([newNote, ...notes]);
+    const notesArray = [newNote, ...notes];
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -36,12 +62,13 @@ export function App() {
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
           type="text"
           placeholder="Busque em suas notas..."
+          onChange={handleSearch}
         />
       </form>
       <div className="h-px bg-slate-700" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />; // Sempre deve ser passado uma key para o objeto, para o react sempre poder identificar cada note
         })}
       </div>
